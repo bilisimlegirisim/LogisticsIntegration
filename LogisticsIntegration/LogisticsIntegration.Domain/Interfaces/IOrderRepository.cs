fe4212bf33105fb4f6e7643246af01384f4ed430
halilibrahimkamat@gmail.com <halilibrahimkamat@gmail.com>
2025-06-21 23:57:40 +0300

    Define core repository and external API client interfaces

---
+++ LogisticsIntegration/LogisticsIntegration.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using LogisticsIntegration.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticsIntegration.Domain.Interfaces
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByStatusAsync(OrderStatus status);
+        Task<IEnumerable<Order>> GetOrdersNotNotifiedToCustomer();
+    }
+}
