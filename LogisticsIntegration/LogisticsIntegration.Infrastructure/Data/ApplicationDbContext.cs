34a7e4555f43913441812b784c66d300368b35ff
halilibrahimkamat@gmail.com <halilibrahimkamat@gmail.com>
2025-06-22 00:15:17 +0300

    Setup Infrastructure project with EF Core and ApplicationDbContext

---
+++ LogisticsIntegration/LogisticsIntegration.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using LogisticsIntegration.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticsIntegration.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public ApplicationDbContext()
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Delivery> Deliveries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(oi => oi.Order)
+                .WithMany(o => o.OrderItems)
+                .HasForeignKey(oi => oi.OrderId);
+
+            modelBuilder.Entity<Delivery>()
+                .HasOne(d => d.Order)
+                .WithMany()
+                .HasForeignKey(d => d.OrderId);
+
+            modelBuilder.Entity<Order>()
+                .Property(o => o.Status)
+                .HasConversion<string>();
+        }
+    }
+}
