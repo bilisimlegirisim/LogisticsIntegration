e72d7954ca551d873af58e2b3f8032f1e762e1b2
halilibrahimkamat@gmail.com <halilibrahimkamat@gmail.com>
2025-06-21 19:50:18 +0300

    Initial solution and project structure

---
+++ LogisticsIntegration/LogisticsIntegration.DeliveryNotifier.Worker/Program.cs
@@ -0,0 +1,10 @@
+using LogisticsIntegration.DeliveryNotifier.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
